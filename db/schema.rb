7fa75beaae49f735856a9daefc82ce4a40790b73
TheStranjer <791672+TheStranjer@users.noreply.github.com>
2026-06-16 02:55:38 -0600

    Use PostgreSQL, not SQLite

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2026_06_16_130000) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.integer "awareness", null: false
     t.integer "charisma", null: false
